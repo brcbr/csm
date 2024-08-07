ec7fc2e2c9700b05b7ea6bdc2841a36603589110
brcbr <brockliam047@gmail.com>
2024-08-07 05:51:33 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-# Ubah direktori ke /home/coder/project
-cd /home/coder/project
-python3 -m venv v 
-source v/bin/activate 
-pip install -r requirements.txt 
-# Mulai code-server tanpa otentikasi pada port 6090
 echo "Starting Web Console..."
 tmate -F &
 # Tunggu selama 10 detik agar code-server siap
-sleep 10
+sleep 5
 apachectl -D FOREGROUND
 
 
