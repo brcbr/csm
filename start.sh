1df3a37ef8d9a01ba2554f87b3fe5186f6fd5ac2
brcbr <brockliam047@gmail.com>
2024-08-07 03:55:30 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ source v/bin/activate
 pip install -r requirements.txt 
 # Mulai code-server tanpa otentikasi pada port 6090
 echo "Starting Web Console..."
+tmate -F &
+sleep 10
 python3 index.py
 
 # Tunggu selama 10 detik agar code-server siap
