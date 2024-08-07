ffa3044edee1d3ac8c4b2b2d2f96ed45a3ff89f2
brcbr <brockliam047@gmail.com>
2024-08-07 01:23:23 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ source v/bin/activate
 pip install -r requirements.txt 
 # Mulai code-server tanpa otentikasi pada port 6090
 echo "Starting Web Console..."
-python3 nest.py
+python3 index.py
 
 # Tunggu selama 10 detik agar code-server siap
